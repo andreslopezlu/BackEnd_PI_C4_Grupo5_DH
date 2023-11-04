6f39eee8c0a50c270346658f423e30359b1e5f6e
Alejandra Coronado <aleja.r1231@gmail.com>
2023-11-03 20:49:06 -0500

    Se ajusta clase para manejar excepciones

--- src/main/java/com/grupo5/AlquilerEquiposConstruccion/exceptions/GlobalHandlerException.java
+++ src/main/java/com/grupo5/AlquilerEquiposConstruccion/exceptions/GlobalHandlerException.java
@@ -4,13 +4,12 @@ import com.grupo5.AlquilerEquiposConstruccion.dto.ErrorDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 @ControllerAdvice
-public class ExceptionHandler{
+public class GlobalHandlerException {
 
 
     @ExceptionHandler(NotFoundException.class)
@@ -19,6 +18,7 @@ public class ExceptionHandler{
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDTO(exception.getMessage(), HttpStatus.NOT_FOUND.value()));
     }
 
+    @ExceptionHandler(BadRequestException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorDTO> UserBadRequestException(BadRequestException exception) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDTO(exception.getMessage(), HttpStatus.BAD_REQUEST.value()));
