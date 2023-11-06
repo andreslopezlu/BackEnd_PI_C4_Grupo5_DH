2ca209c81e3577a2ac577c179d2de334f2b728ed
andreslopezlu <andreslopezlu@gmail.com>
2023-11-06 18:19:06 -0500

    Se configuran requestMatchers básicos

--- src/main/java/com/grupo5/AlquilerEquiposConstruccion/security/config/SecurityConfig.java
+++ src/main/java/com/grupo5/AlquilerEquiposConstruccion/security/config/SecurityConfig.java
@@ -2,11 +2,13 @@ package com.grupo5.AlquilerEquiposConstruccion.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 public class SecurityConfig {
@@ -16,8 +18,15 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(antMatcher(HttpMethod.GET, "/**")).permitAll()
+                        .requestMatchers(antMatcher(HttpMethod.POST, "/**")).permitAll()
+                        .requestMatchers(antMatcher(HttpMethod.PUT, "/**")).permitAll()
+                        .requestMatchers(antMatcher(HttpMethod.DELETE, "/**")).permitAll()
                         .anyRequest().authenticated()
-                ).httpBasic(withDefaults());
+                )
+                .httpBasic(withDefaults());
         return http.build();
     }
+
+
 }
