71c4960c9f1bdaf13522ee8b06b0ed9bf6c14ea5
Alejandra Coronado <aleja.r1231@gmail.com>
2023-11-10 12:23:46 -0500

    Se ajusta permisos para endpoints de categorias y productos

--- src/main/java/com/grupo5/AlquilerEquiposConstruccion/security/config/SecurityConfig.java
+++ src/main/java/com/grupo5/AlquilerEquiposConstruccion/security/config/SecurityConfig.java
@@ -40,6 +40,8 @@ public class SecurityConfig {
                         .requestMatchers(antMatcher(HttpMethod.PUT, "/user/update/*")).hasRole("ADMIN")
                                 .requestMatchers(antMatcher("/user/create")).permitAll()
                         .requestMatchers(antMatcher("/api/auth/**")).permitAll()
+                        .requestMatchers(antMatcher("/categories")).permitAll()
+                        .requestMatchers(antMatcher("/products/**")).permitAll()
                         .requestMatchers(antMatcher(HttpMethod.GET, "/**")).hasAnyRole("ADMIN", "USER")
                         .requestMatchers(antMatcher(HttpMethod.POST, "/**")).hasRole("ADMIN")
                         .requestMatchers(antMatcher(HttpMethod.PUT, "/**")).hasRole("ADMIN")
