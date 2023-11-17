81132890f5c967a67e563d9916b418a31f7b8313
andreslopezlu <andreslopezlu@gmail.com>
2023-11-17 14:19:59 -0500

    Se configura la clase S3Controller

---
+++ src/main/java/com/grupo5/AlquilerEquiposConstruccion/controller/S3Controller.java
@@ -0,0 +1,71 @@
+package com.grupo5.AlquilerEquiposConstruccion.controller;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.grupo5.AlquilerEquiposConstruccion.service.impl.S3ServiceImpl;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+
+@RestController
+@RequestMapping("/file")
+public class S3Controller {
+
+    @Autowired
+    private S3ServiceImpl s3ServiceImpl;
+
+    @Value("${aws.s3.bucketName}")
+    private String bucketName;
+
+    AWSCredentials credentials = new BasicAWSCredentials(
+            "AKIAY3PLHSUJCOQE5HPE",
+            "edla3tGu+0R/Yz4tUlT8eQETIKFFuN1u4Ph0md6t"
+    );
+
+    private AmazonS3 amazonS3 = AmazonS3ClientBuilder
+            .standard()
+            .withCredentials(new AWSStaticCredentialsProvider(credentials))
+            .withRegion(Regions.US_WEST_1)
+            .build();
+
+    public S3Controller(@Value("${aws.s3.bucketName}") String bucketName) {
+        this.bucketName = bucketName;
+    }
+
+
+    @PostMapping("/upload/{id}")
+    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file, @PathVariable Integer id) throws IOException {
+
+        File convertedFile = convertMultiPartFileToFile(file);
+        String fileName = generateFileName(file);
+        s3ServiceImpl.uploadFile(fileName, convertedFile);
+        convertedFile.delete();
+
+        String s3Url = amazonS3.getUrl(bucketName, fileName).toString();
+        return ResponseEntity.ok(s3Url);
+    }
+
+
+    private File convertMultiPartFileToFile(MultipartFile file) throws IOException {
+        File convertedFile = new File(file.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(convertedFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convertedFile;
+    }
+
+    private String generateFileName(MultipartFile multiPart) {
+        return new Date().getTime() + "-" + multiPart.getOriginalFilename().replace(" ", "_");
+    }
+}
--- src/main/java/com/grupo5/AlquilerEquiposConstruccion/service/S3Service.java
+++ src/main/java/com/grupo5/AlquilerEquiposConstruccion/service/S3Service.java
@@ -1,37 +1,14 @@
 package com.grupo5.AlquilerEquiposConstruccion.service;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.S3Object;
 
 import java.io.File;
 
-@Service
-public class S3Service {
+public interface S3Service {
 
+    void uploadFile(String key, File file);
 
-    private final AmazonS3 amazonS3;
-    private final String bucketName;
+    S3Object downloadFile(String key);
 
-
-    public S3Service(AmazonS3 amazonS3, @Value("${aws.s3.bucketName}") String bucketName) {
-        this.amazonS3 = amazonS3;
-        this.bucketName = bucketName;
-    }
-
-
-    public void uploadFile(String key, File file) {
-        amazonS3.putObject(bucketName, key, file);
-    }
-
-
-    public void deleteFile(String key) {
-        amazonS3.deleteObject(bucketName, key);
-    }
-
-
-    public S3Object downloadFile(String key) {
-        return amazonS3.getObject(bucketName, key);
-    }
+    void deleteFile(String key);
 }
